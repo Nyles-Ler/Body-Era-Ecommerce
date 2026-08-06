31512875b1dee36ef9cd4f34a0582d69c81c339a
Jordan Lerat <jlerat@rrc.ca>
2026-08-06 12:39:12 -0500

    Integrated stripe payment redirect

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,18 +1,15 @@
 ActiveAdmin.register Order do
+  permit_params :order_status
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :user_id, :subtotal, :tax_amount, :total_amount, :order_status, :address_id, :gst_rate, :pst_rate, :hst_rate, :gst_amount, :pst_amount, :hst_amount
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:user_id, :subtotal, :tax_amount, :total_amount, :order_status, :address_id, :gst_rate, :pst_rate, :hst_rate, :gst_amount, :pst_amount, :hst_amount]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  remove_filter :stripe_checkout_session_id
+  remove_filter :stripe_payment_intent_id
+
+  filter :user
+  filter :address
+  filter :order_status
+  filter :subtotal
+  filter :tax_amount
+  filter :total_amount
+  filter :created_at
+
 end
